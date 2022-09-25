819a16708a8f88516f724271d5bd51c11e28855f
Crzy-iD <113808384+Crzy-iD@users.noreply.github.com>
2022-09-25 09:43:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -59,7 +59,7 @@ global.lolkey = '8e66d0934cf741bfd2182c16' // PUNYA WAHID 357d3c85079c4e0818731e
 /*============== STAFF ==============*/
 global.owner = [
   ['6285814417473', 'WH MODS DEV', true],
-  ['-', 'isi_kalau_ada', true],
+  ['-', '6281212939255', 'Store', true],
   ['-', 'isi_kalau_ada', true],
   ['-', 'isi_kalau_ada', true] //Ganti jd nomormu sama Namamu
   // [number, dia creator/owner?, dia developer?]
