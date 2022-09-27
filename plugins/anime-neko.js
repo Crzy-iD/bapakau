1e3d2508c5634e2955f7d2cc10b9ba392419cf9c
Crzy-iD <113808384+Crzy-iD@users.noreply.github.com>
2022-09-27 09:09:26 +0700

    Update anime-neko.js

--- plugins/anime-neko.js
+++ plugins/anime-neko.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 	let res = await fetch(`https://api.waifu.pics/sfw/${command}`)
 	if (!res.ok) throw await res.text()
 	let json = await res.json()
-	conn.sendButton(m.chat, `Nyaww~ 🐾💗 ${command.capitalize()}`, json.url, json.url, [['Next', usedPrefix + command]], m)
+	conn.sendButton(m.chat, `Pedo~🗿  ${command.capitalize()}`, json.url, json.url, [['Next', usedPrefix + command]], m)
 }
 handler.command = /^(neko)$/i
 handler.tags = ['anime']
