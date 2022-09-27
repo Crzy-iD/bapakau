c879b3cb4d5f4a6181a96b74463ed9b5b3124f6c
Crzy-iD <113808384+Crzy-iD@users.noreply.github.com>
2022-09-28 00:50:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -119,11 +119,11 @@ global.author = '                「 Zen-Bot  あ」'
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://i.ibb.co/XZrK6yQ/transformers.jpg' //Main Thumbnail
-global.imagebot = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
+global.thumb = 'https://i.ibb.co/9r1NRM1/qr.png' //Main Thumbnail
+global.imagebot = 'https://i.ibb.co/9r1NRM1/qr.png'
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
-global.thumbs = ['https://i.ibb.co/XZrK6yQ/transformers.jpg']
-global.fla = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
+global.thumbs = ['https://i.ibb.co/9r1NRM1/qr.png']
+global.fla = 'https://i.ibb.co/9r1NRM1/qr.png'
 global.flaaa2 = [
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
